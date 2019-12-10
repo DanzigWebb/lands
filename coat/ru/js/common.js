cf282045b6a15dc5a1a7e05e654f51f8f86d9ed3
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2019-12-10 23:44:11 +0300

    c

--- coat/ru/js/common.js
+++ coat/ru/js/common.js
@@ -6,10 +6,12 @@ var galleryThumbs = new Swiper('.gallery-thumbs', {
   loopedSlides: 5, //looped slides should be the same
   watchSlidesVisibility: true,
   watchSlidesProgress: true,
+  autoplay: true
 });
 var galleryTop = new Swiper('.gallery-top', {
   spaceBetween: 10,
   loop: true,
+  autoplay: true,
   loopedSlides: 5, //looped slides should be the same
   thumbs: {
     swiper: galleryThumbs,
