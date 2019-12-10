271197f5df0c44b793ea2b3f5af9e72dc0074dbe
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2019-12-10 13:04:35 +0300

    с

---
+++ ideal-skin/gr/js/common.js
@@ -0,0 +1,73 @@
+
+
+
+window.onload = function () {
+
+  lazyScroll();
+
+  validateInput();
+
+  pulseBtn();
+  // ========================>
+  // slider for reviews
+  // <=======================
+  var swiper = new Swiper('.reviews__slider', {
+    pagination: {
+      el: '.reviews__pagination',
+      clickable: true,
+    },
+    // loop: true,
+    autoHeight: true,
+    spaceBetween: 40
+  });
+}
+
+
+// ========================>
+// validate input
+// <=======================
+function validateInput() {
+  var input = document.querySelector('input[name="name"]');
+  input.addEventListener('input', function () {
+    input.value = input.value.replace(/[0-9]/g, '');
+  });
+}
+// ========================>
+// animation scroll
+// <=======================
+function lazyScroll() {
+  var linkNav = document.querySelectorAll('[href^="#"]'), V = .2;
+  for (var i = 0; i < linkNav.length; i++) {
+    linkNav[i].addEventListener('click', function (e) {
+      e.preventDefault();
+      var w = window.pageYOffset, hash = this.href.replace(/[^#]*(.*)/, '$1');
+      t = document.querySelector(hash).getBoundingClientRect().top,
+        start = null;
+      requestAnimationFrame(step);
+      function step(time) {
+        if (start === null)
+          start = time;
+        var progress = time - start, r = (t < 0 ? Math.max(w - progress / V, w + t) : Math.min(w + progress / V, w + t));
+        window.scrollTo(0, r);
+        if (r != w + t) {
+          requestAnimationFrame(step);
+        }
+        else {
+          location.hash = hash;
+        }
+      }
+    }, false);
+  }
+  ;
+}
+// ========================>
+// pulse btn
+// <=======================
+function pulseBtn() {
+  var offer = document.getElementById('footer');
+  var btn = document.querySelector('.pulse__btn');
+  window.addEventListener('scroll', (e) => {
+    document.documentElement.scrollTop + 300 > offer.offsetTop ? btn.classList.add('show') : btn.classList.remove('show');
+  });
+}
+
