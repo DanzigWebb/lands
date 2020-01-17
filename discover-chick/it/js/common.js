fcd1de574f3f8308249da6872ba52522c5edf76c
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2020-01-17 18:07:38 +0300

    с

--- discover-chick/it/js/common.js
+++ discover-chick/it/js/common.js
@@ -4,16 +4,58 @@ const likeControl = () => {
     const [index, like] = item.children;
     const [img] = like.children;
     like.addEventListener('click', () => {
-      index.innerHTML = 1;
-      img.src = 'img/heart1.svg';
+      if (index.innerHTML) {
+        index.innerHTML = '';
+        img.src = 'img/heart.svg';
+      }
+      else {
+        index.innerHTML = 1;
+        img.src = 'img/heart1.svg';
+      }
+
     });
   });
+};
+
+likeControl()
+// showPost()
+
+const showPost = () => {
+  const posts = $('.post-and-preview');
+  const showAllBtn = $('.blog__all-posts');
+  const preloader = $('.preloader')
+  const blog = $('.blog');
+
+  const hideAllElements = (elems) => {
+    elems.each((index, item) => {
+      $(item).fadeOut()
+    })
+  };
+  const showAllPosts = () => {
+    posts.each((i, item) => {
+      const [posts, preview] = $(item).children();
+      $(posts).fadeOut();
+      $(preview).fadeIn()
+    })
+  }
+
+  // listeners
+  showAllBtn.on('click', () => showAllPosts())
+
+  posts.on('click', function (e) {
+    const { target } = e;
+    const validTarget = target.matches('.post-heading__title') || target.matches('.post__section--preview') ? true : false
+    if (validTarget) {
+      const [post] = $(this).children();
+      preloader.fadeIn()
+      hideAllElements($('article.post'));
+      setTimeout(() => {
+        $(post).show()
+        preloader.fadeOut();
+      }, 500);
+      window.scrollTo(0, blog.offset().top)
+    }
+  })
 }
 
-var vm = new Vue({
-  el: '#app',
-  
-  mounted() {
-    likeControl()
-  },
-})
+showPost()
