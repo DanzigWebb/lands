09c55e8315599aa2efeb0dc3834ec9f0306c1a95
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2020-01-28 23:14:43 +0300

    c

--- call-center/ru/js/common.js
+++ call-center/ru/js/common.js
@@ -6,6 +6,12 @@ $('.reviews').slick({
   slidesToShow: 1
 });
 
+$("body").on('click', '[href*="#"]', function (e) {
+  var fixed_offset = 100;
+  $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+  e.preventDefault();
+});
+
 var hiddenText = $('.hidden-content');
 var accItems = $('.block9__item');
 function showAccItems() {
