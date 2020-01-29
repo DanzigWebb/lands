7cf68c3564239fd2713d34f16aa4c1a373c2cb3c
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2020-01-29 17:12:40 +0300

    icon

---
+++ icon/ru/js/common.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $(".reviews").owlCarousel({
+    margin: 10,
+    items: 1,
+    nav: true,
+    autoHeight: true,
+    navText: ['<img src="img/left.png" alt="">', '<img src="img/right.png" alt="">']
+  });
+});
+
+$("body").on('click', '[href*="#"]', function (e) {
+  var fixed_offset = 100;
+  $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+  e.preventDefault();
+});
