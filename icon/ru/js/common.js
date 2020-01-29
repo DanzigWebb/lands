9f92836a9d13947ade48cfbfba87f20fc10a35fc
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2020-01-29 22:13:54 +0300

    icon complete

--- icon/ru/js/common.js
+++ icon/ru/js/common.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
   $(".reviews").owlCarousel({
     margin: 10,
     items: 1,
+    dots: false,
     nav: true,
     autoHeight: true,
     navText: ['<img src="img/left.png" alt="">', '<img src="img/right.png" alt="">']
@@ -13,3 +14,10 @@ $("body").on('click', '[href*="#"]', function (e) {
   $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
   e.preventDefault();
 });
+
+const timer = new minTimer({
+  hour: 't-hour',
+  min: 't-min',
+  sec: 't-sec',
+  separation: true
+}).start();
