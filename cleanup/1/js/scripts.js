9d9c7c6d61282e9f07eba712c648ed0a356a147c
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2020-01-30 22:50:13 +0300

    с

--- cleanup/1/js/scripts.js
+++ cleanup/1/js/scripts.js
@@ -1,10 +1,10 @@
-$(document).ready(function() {
-	
+$(document).ready(function () {
+
 	/* scroll */
-	
-	$("a[href^='#']").click(function(){
+
+	$("a[href^='#']").click(function () {
 		var _href = $(this).attr("href");
-		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+		$("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
 		return false;
 	});
 
@@ -12,24 +12,24 @@ $(document).ready(function() {
 
 	function update() {
 		var Now = new Date(), Finish = new Date();
-		Finish.setHours( 23);
-		Finish.setMinutes( 59);
-		Finish.setSeconds( 59);
-		if( Now.getHours() === 23  &&  Now.getMinutes() === 59  &&  Now.getSeconds === 59) {
-			Finish.setDate( Finish.getDate() + 1);
+		Finish.setHours(23);
+		Finish.setMinutes(59);
+		Finish.setSeconds(59);
+		if (Now.getHours() === 23 && Now.getMinutes() === 59 && Now.getSeconds === 59) {
+			Finish.setDate(Finish.getDate() + 1);
 		}
-		var sec = Math.floor( ( Finish.getTime() - Now.getTime()) / 1000);
-		var hrs = Math.floor( sec / 3600);
+		var sec = Math.floor((Finish.getTime() - Now.getTime()) / 1000);
+		var hrs = Math.floor(sec / 3600);
 		sec -= hrs * 3600;
-		var min = Math.floor( sec / 60);
+		var min = Math.floor(sec / 60);
 		sec -= min * 60;
-		$(".timer .hours").html( pad(hrs));
-		$(".timer .minutes").html( pad(min));
-		$(".timer .seconds").html( pad(sec));
-		setTimeout( update, 200);
+		$(".timer .hours").html(pad(hrs));
+		$(".timer .minutes").html(pad(min));
+		$(".timer .seconds").html(pad(sec));
+		setTimeout(update, 200);
 	}
 	function pad(s) {
-		s = ("00"+s).substr(-2);
+		s = ("00" + s).substr(-2);
 		return "<span>" + s[0] + "</span><span>" + s[1] + "</span>";
 	}
 	update();
@@ -38,20 +38,20 @@ $(document).ready(function() {
 
 
 
-	$(window).on("load", function(){
-		/* sliders */
-		$(".owl-carousel").owlCarousel({
-			items: 1,
-			loop: true,
-			smartSpeed: 300,
-			mouseDrag: false,
-			pullDrag: false,
-			autoHeight: true,
-			dots: false,
-			nav: true,
-			autoplay:true,
-	    autoplayTimeout:2000,
-	    autoplayHoverPause:true,
-			navText: ""
-		});
+$(window).on("load", function () {
+	/* sliders */
+	$(".owl-carousel").owlCarousel({
+		items: 1,
+		loop: true,
+		smartSpeed: 300,
+		mouseDrag: false,
+		pullDrag: false,
+		autoHeight: true,
+		dots: false,
+		nav: true,
+		autoplay: true,
+		autoplayTimeout: 2000,
+		autoplayHoverPause: true,
+		navText: ""
 	});
+});
