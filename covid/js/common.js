2008ba3bfce888b0d15cd7cf69ae06c742625215
Alexandr <valverin95@gmail.com>
2020-04-19 19:49:52 +0300

    D

--- covid/js/common.js
+++ covid/js/common.js
@@ -36,3 +36,36 @@ $(document).ready(function () {
     autoHeight: true,
   });
 });
+
+
+var hiddenText = $('.hidden-content');
+var accItems = $('.acca__item');
+var ticket = $('.ticket');
+
+function showAccItems() {
+  hiddenText.slideUp(400);
+  accItems.removeClass('active');
+  ticket.html('<img src="img/plus.svg" alt="">')
+}
+accItems.each(function (index) {
+  $(this).on('click', function () {
+    if ($(this).hasClass('active')) return
+    showAccItems();
+    $(hiddenText[index]).slideDown(400);
+    $(ticket[index]).html('<img src="img/minus.svg" alt="">')
+    $(this).addClass('active');
+  });
+});
+
+
+$('.header__btn').on('click', function() {
+  $('.mob__nav').slideDown(200);
+})
+
+$('.mob__link').on('click', function() {
+  $('.mob__nav').slideUp(200);
+})
+
+$('.mob__nav-close').on('click', function() {
+  $('.mob__nav').slideUp(200);
+})
