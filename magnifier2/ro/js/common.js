8f36354189d1d5165f28d09fd8df915b6c675736
Alexandr <valverin95@gmail.com>
2020-04-28 20:12:59 +0300

    v

---
+++ magnifier2/ro/js/common.js
@@ -0,0 +1,27 @@
+
+
+$("body").on('click', '[href*="#"]', function (e) {
+  var fixed_offset = 100;
+  $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+  e.preventDefault();
+});
+
+const timer = new minTimer({
+  hour: 't-hour',
+  min: 't-min',
+  sec: 't-sec',
+  separation: true
+}).start();
+
+$(document).ready(function () {
+  $(".reviews").owlCarousel({
+    margin: 20,
+    items: 1,
+    dots: true,
+    autoHeight: true
+  });
+});
+
+
+const b = document.querySelector('.sect2 p b');
+b.style.cssText = 'display: block; margin-bottom: 0.1rem';
