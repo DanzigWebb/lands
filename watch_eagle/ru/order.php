cf070637b9bf621eb40ef5cff7952d0b3a2dfebe
Alexandr <valverin95@gmail.com>
2020-05-31 21:49:46 +0300

    v

--- watch_eagle/ru/order.php
+++ watch_eagle/ru/order.php
@@ -1,51 +1,47 @@
 <?php
 
-if (!empty($_POST['name']) && !empty($_POST['phone'])) {
+include_once './lib/cURL.php';
 
-    $params = array(
-        'ref'     => '34', //ID-вебмастера в системе
-        'api_key'    => '3eca310d0010050ac5c359e8d8485c13', //API-ключ из профиля вебмастера
-        'product_id'    => '999', // ID товара в системе M1-shop
-        'name'       => $_POST['name'],
-        'phone'      => $_POST['phone'],
-        'address'    => $_POST['address'],
-        'referrer'   => isset($_SERVER["HTTP_REFERER"]) ? $_SERVER["HTTP_REFERER"] : "",
-        'ip'         => isset($_SERVER["REMOTE_ADDR"]) ? $_SERVER["REMOTE_ADDR"] : "",
-        's'       => '23gg', //взять из задачи
-        'w'       => $_POST['sub2'],
-        't'       => $_POST['sub3']
-    );
+define('TOKEN', '5798ac6061c17003610065518'); // Токен вебмастера
+define('CAMPAIGN_HASH', 'fdf'); // Хеш потока
 
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'http://m1-shop.ru/send_order/');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
-    $result = curl_exec($ch);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $userAgent = $_SERVER['HTTP_USER_AGENT'];
+    $utm_source = $_POST['utm_source'];
+    $utm_content = $_POST['utm_content'];
+    $utm_campaign = $_POST['utm_campaign'];
+    $utm_term = $_POST['utm_term'];
+    $utm_medium = $_POST['utm_medium'];
+    $subid = $_POST['subid']; //в статистике ПП этот параметр не отображается, но возвращется в постбеке
+    $subid1 = $_POST['subid1'];
+    $subid2 = $_POST['subid2'];
+    $subid3 = $_POST['subid3'];
+    $country_call = 'sfd'; //страна для обзвона
 
-    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    $data = [
+        'campaignHash' => CAMPAIGN_HASH,
+        'ip' => $ip,
+        'name' => $name,
+        'phone' => $phone,
+        'userAgent' => $userAgent,
+        'country_call' => $country_call
+    ];
 
-    curl_close($ch);
+    $curl = new cURL;
+    $response = $curl->post('https://lucky.online/api-webmaster/lead.html?subid1=' . $subid1 . '&subid2=' . $subid2 . '&subid3=' . $subid3 . '&subid=' . $subid .'&utm_source=' . $utm_source . '&utm_content=' . $utm_content . '&utm_campaign=' . $utm_campaign . '&utm_term=' . $utm_term . '&utm_medium=' . $utm_medium . '&api_key=' . TOKEN,
+        $data);
 
-    if ($http_code == 200) {
+    $body = json_decode($response->body);
 
-        // Success
+    if ($body->success) {
         header('Location: ' . 'confirm.html');
-        exit;
+        exit();
     } else {
-
-        //закомментить для дебага
-
-        header('Location: ' . 'confirm.html');
-        exit;
-
-        //раскомментить для дебага
-
-        //$result = json_decode($result);
-        //echo $result->error;
-        //exit;
-
+        var_dump($body);
+        exit();
     }
 }
+
