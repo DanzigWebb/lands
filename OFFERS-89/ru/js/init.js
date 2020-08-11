a1f27020c2046da56650444380f6b115c56906b7
Alexandr <valverin95@gmail.com>
2020-08-11 20:03:57 +0300

    М

---
+++ OFFERS-89/ru/js/init.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+
+    $('a[href^="#"]').click(function () {
+        var elementClick = $(this).attr("href");
+        var destination = $(elementClick).offset().top;
+        jQuery("html:not(:animated), body:not(:animated)").animate({scrollTop: destination}, 600);
+        return false;
+    });
+
+    /* timer */
+    now = new Date();
+    var perem = (24-now.getHours())*3600;
+    $(".el-timer").attr("data-timer", perem);
+    $(".el-timer").TimeCircles({
+        "animation": "smooth",
+        "bg_width": 0.2,
+        "fg_width": 0.04,
+        "circle_bg_color": "#fafafa",
+        "time": {
+            "Days": {
+                "text": "Days",
+                "color": "#fff600",
+                "show": false
+            },
+            "Hours": {
+                "text": "Часов",
+                "color": "#fff600",
+                "show": true
+            },
+            "Minutes": {
+                "text": "Минут",
+                "color": "#fff600",
+                "show": true
+            },
+            "Seconds": {
+                "text": "Секунд",
+                "color": "#fff600",
+                "show": true
+            }
+        }
+    });
+
+});
+
