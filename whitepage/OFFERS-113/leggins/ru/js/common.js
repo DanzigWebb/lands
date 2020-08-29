9c19b658804a49c6e582ec7f99287b438051f90b
Alexandr <valverin95@gmail.com>
2020-08-29 18:37:09 +0300

    b

---
+++ whitepage/OFFERS-113/leggins/ru/js/common.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+  newsItemsAutoHeight()
+}
+
+function newsItemsAutoHeight() {
+  const items = document.querySelectorAll('.products__item')
+  const [first] = items
+
+  setHeight()
+
+  window.addEventListener('resize', setHeight)
+
+  function setHeight() {
+    items.forEach(item => item.style.height = first.offsetWidth + 'px')
+  }
+}
