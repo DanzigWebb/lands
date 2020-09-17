a06f73a19266a6b68354c36866b594c566b1f845
Kaflilithe <kudiyarova.1975@gmail.com>
2020-09-17 10:44:56 +0300

    k

---
+++ pre/OFFERS-120/js/common.js
@@ -0,0 +1,165 @@
+window.onload = function () {
+  initDateComments()
+  autoImage()
+  navbar()
+  setAva()
+  scale()
+  wheel()
+}
+
+
+
+function initDateComments() {
+  const getDay = (num) => {
+    let now = new Date();
+    let newDate = new Date(now.setDate(now.getDate() - num));
+    return `${('0' + newDate.getDate()).slice(-2)}.${('0' + newDate.getMonth()).slice(-2)}.${newDate.getFullYear()}`
+  }
+
+  const commentsDate = document.querySelectorAll('.get-date');
+  commentsDate.forEach(item => {
+    item.innerHTML = getDay(item.getAttribute('data-day-delay'))
+  })
+}
+
+function autoImage() {
+  const images = document.querySelectorAll('.img img')
+  images.forEach(img => {
+    img.style.width = '100%'
+    const width = img.naturalWidth || ' '
+    img.parentNode.style.maxWidth = width + 'px'
+  })
+}
+
+function navbar() {
+  const btn = document.querySelector('.header__mob');
+  const navbarRef = document.querySelector('.navbar');
+  const overlay = document.querySelector('.overlay')
+
+  btn.addEventListener('click', () => {
+
+
+    (navbarRef.matches('.active'))
+      ? hide()
+      : show()
+  })
+
+
+  function show() {
+    overlay.classList.add('active')
+    navbarRef.classList.add('active')
+    overlay.addEventListener('click', () => hide(), { once: true })
+  }
+
+  function hide() {
+    overlay.classList.remove('active')
+    navbarRef.classList.remove('active')
+  }
+
+}
+
+function setAva() {
+  const commentsAva = document.querySelectorAll('.comment__head img')
+  commentsAva.forEach((ava, index) => {
+    ava.setAttribute('src', `img/ava${index + 1}.png`)
+  })
+}
+
+function scale() {
+
+  const btns = document.querySelectorAll('.calculator__scale button');
+  btns.forEach(item => (
+    item.addEventListener('click', handler)
+  ))
+
+  function handler() {
+    reset()
+    this.classList.add('active')
+  }
+
+  function reset() {
+    btns.forEach(btn => btn.classList.remove('active'))
+  }
+}
+const timer = new minTimer({
+  hour: 't-hour',
+  min: 't-min',
+  sec: 't-sec',
+  separation: true
+}).start();
+
+
+
+$("body").on('click', '[href*="#"]', function (e) {
+  var fixed_offset = 100;
+  $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+  e.preventDefault();
+});
+
+$(document).ready(function () {
+  var rev = $(".reviews").owlCarousel({
+    margin: 20,
+    items: 3,
+    dots: true,
+    nav: false,
+    autoHeight: true,
+
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
+    }
+  });
+
+});
+$(document).ready(function () {
+  var rev = $(".reviews2").owlCarousel({
+    margin: 20,
+    items: 1,
+    dots: true,
+    nav: false,
+    navText: ['<img src="img/arl.svg"></img>', '<img src="img/arr.svg"></img>'],
+    autoHeight: true,
+
+    responsive: {
+      0: {
+        items: 1 
+      },
+      1100: {
+        items: 1,
+        nav: true,
+      }
+    }
+  });
+
+});
+function showAlert() {
+  const [title, description, button] = document.querySelector('.sweet-text-hidden').children;
+
+  swal({
+    title: title.textContent,
+    text: description.textContent,
+    button: button.textContent,
+    icon: "success",
+  }).then(_ => window.open(currentLink, '_self'));
+
+  setTimeout(() => {
+    window.open(currentLink, '_self')
+  }, 2500);
+}
+function wheel() {
+  const btn = document.querySelector('.wheel-cursor');
+  const img = document.querySelector('.wheel-img');
+  btn.addEventListener('click', () => {
+    img.classList.add('super-rotation');
+    setTimeout(() => {
+      showAlert()
+    }, 7500);
+  })
+}
