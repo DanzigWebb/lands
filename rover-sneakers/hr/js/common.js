64e1fbf12001c4940b24d78e928bcdca274022f5
Kaflilithe <kudiyarova.1975@gmail.com>
2020-09-25 16:33:29 +0300

    j

---
+++ rover-sneakers/hr/js/common.js
@@ -0,0 +1,69 @@
+window.onload = function() {
+// ========================>
+// slider for reviews
+// <=======================swiper.update()
+var swiper = new Swiper('.reviews__slider', {
+  pagination: {
+    el: '.reviews__pagination',
+    clickable: true,
+  },
+  // loop: true,
+  autoHeight: true,
+  spaceBetween: 40
+});
+// ========================>
+// animation scroll
+// <=======================
+(function () {
+  var linkNav = document.querySelectorAll('[href^="#"]'),
+    V = .2;
+  for (var i = 0; i < linkNav.length; i++) {
+    linkNav[i].addEventListener('click', function (e) {
+      e.preventDefault();
+      var w = window.pageYOffset,
+        hash = this.href.replace(/[^#]*(.*)/, '$1');
+      t = document.querySelector(hash).getBoundingClientRect().top,
+        start = null;
+      requestAnimationFrame(step);
+      function step(time) {
+        if (start === null) start = time;
+        var progress = time - start,
+          r = (t < 0 ? Math.max(w - progress / V, w + t) : Math.min(w + progress / V, w + t));
+        window.scrollTo(0, r);
+        if (r != w + t) {
+          requestAnimationFrame(step)
+        } else {
+          location.hash = hash
+        }
+      }
+    }, false);
+  };
+  // ========================>
+  // validate input
+  // <=======================
+  var input = document.querySelector('input[name="name"]');
+  input.addEventListener('input', function () {
+    input.value = input.value.replace(/[0-9]/g, '')
+  });
+  // ========================>
+  // pulse btn
+  // <=======================
+  var offer = document.getElementById('offer');
+  var btn = document.querySelector('.pulse__btn');
+  window.addEventListener('scroll', (e) => {
+    document.documentElement.scrollTop + 300 > offer.offsetTop ? btn.classList.add('show') : btn.classList.remove('show')
+  })
+
+})();
+
+
+let mainDots = document.querySelectorAll('.main__dot')
+let count = 0.2
+mainDots.forEach(item => {
+  item.style.opacity = count
+  count += 0.2
+})
+
+
+  swiper.update()
+}
