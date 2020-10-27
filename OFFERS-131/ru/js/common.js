853702079c4fda1ca414e5395c335a87420f4572
Kaflilithe <kudiyarova.1975@gmail.com>
2020-10-27 18:06:12 +0300

    k

---
+++ OFFERS-131/ru/js/common.js
@@ -0,0 +1,34 @@
+
+
+$("body").on('click', '[href*="#"]', function (e) {
+  var fixed_offset = 100;
+  $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+  e.preventDefault();
+});
+
+const timer = new minTimer({
+  hour: 't-hour',
+  min: 't-min',
+  sec: 't-sec',
+  separation: true
+}).start();
+
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    margin: 20,
+    items: 1,
+    dots: true,
+    nav: false,
+    autoHeight: true
+  });
+});
+
+
+// new CircleType(document.querySelector('.text_circle'));
+textRadius()
+function textRadius() {
+  const text = document.querySelectorAll('.text_circle');
+  text.forEach(item => {
+    new CircleType(item)
+  })
+}
