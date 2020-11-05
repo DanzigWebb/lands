f47dfb17a0b6ebb61815ad3260043c1133a0385d
Kaflilithe <kudiyarova.1975@gmail.com>
2020-11-05 12:38:10 +0300

    h

--- OFFERS-130/js/common.js
+++ OFFERS-130/js/common.js
@@ -1,26 +1,15 @@
 window.onload = function () {
-  initDateComments()
   autoImage()
   navbar()
-  setAva()
-  wheel()
 }
 
+$("body").on('click', '[href*="#"]', function (e) {
+  var fixed_offset = 100;
+  $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+  e.preventDefault();
+});
 
 
-function initDateComments() {
-  const getDay = (num) => {
-    let now = new Date();
-    let newDate = new Date(now.setDate(now.getDate() - num));
-
-    return `${('0' + newDate.getDate()).slice(-2)}.${('0' + (newDate.getMonth() + 1)).slice(-2)}.${newDate.getFullYear()}`
-  }
-
-  const commentsDate = document.querySelectorAll('.get-date');
-  commentsDate.forEach(item => {
-    item.innerHTML = getDay(item.getAttribute('data-day-delay'))
-  })
-}
 
 function autoImage() {
   const images = document.querySelectorAll('.img img')
@@ -57,15 +46,6 @@ function navbar() {
   }
 
 }
-
-function setAva() {
-  const commentsAva = document.querySelectorAll('.comment__head img')
-  commentsAva.forEach((ava, index) => {
-    ava.setAttribute('src', `img/ava${index + 1}.png`)
-  })
-}
-
-
 const timer = new minTimer({
   hour: 't-hour',
   min: 't-min',
